b11890d5a598431180019d97ae4cc298f473d146
loickGombauld <wild1907@hotmail.fr>
2022-05-06 15:06:45 +0200

    Ajout d'une fenetre editeur pour les tuiles

--- jeu-de-stratégie-tactique/Assets/Script/Loïck/Editor/Tiles Selector windows.cs
+++ jeu-de-stratégie-tactique/Assets/Script/Loïck/Editor/Tiles Selector windows.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
 using UnityEditor;
 using UnityEngine;
 
-public class TilesSelectorwindows : OdinMenuEditorWindow
+public class TilesSelector : OdinEditorWindow
 {
     [MenuItem("Cassoulet Tool/Tiles Selector")]
-    protected override OdinMenuTree BuildMenuTree()
+    private static void OpenWindow()
     {
+        GetWindow<TilesSelector>().Show();
+    }
 
-        var tree = new OdinMenuTree(); ;
-        tree.AddAllAssetsAtPath("", "Assets/Prefabs/Tiles", typeof(GameObject));
+    public TileList Editor;
+}
 
-        return tree;
+[System.Serializable]
+public class TileList
+{
+    [AssetList(Path = "Prefabs/Tiles")]
+    [PreviewField(70, Sirenix.OdinInspector.ObjectFieldAlignment.Center)]
+    public GameObject SomePrefab;
+    [TableColumnWidth(160)]
+    [Button("Selection Tile", ButtonSizes.Medium),GUIColor(0,1,0,1)]
+    public void PickupThePrefab()
+    {
+        GridInit gridInit = GameObject.FindGameObjectWithTag("Grid Editor").GetComponent<GridInit>();
+        gridInit.currentTilesRef = SomePrefab;
     }
 }
