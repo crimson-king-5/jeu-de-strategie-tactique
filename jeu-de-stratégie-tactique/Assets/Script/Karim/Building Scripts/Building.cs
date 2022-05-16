9270f2c448736591868e6bfd64f6e7d8afd7b440
Karim <karimal-homsi@hotmail.com>
2022-05-16 19:57:25 +0200

    building system 90% finished

--- jeu-de-stratégie-tactique/Assets/Script/Karim/Building Scripts/Building.cs
+++ jeu-de-stratégie-tactique/Assets/Script/Karim/Building Scripts/Building.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+    public bool Placed { get; private set; }
+    public BoundsInt area;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +16,29 @@ public class Building : MonoBehaviour
     }
 
     #region Buil Metods
+
+    public bool CanBePlaced()
+    {
+        Vector3Int positionInt = GridBuildingSystem.current.gridLayout.LocalToCell(transform.position);
+        BoundsInt areaTemp = area;
+        areaTemp.position = positionInt;
+
+        if (GridBuildingSystem.current.CanTakeArea(areaTemp))
+        {
+            return true;
+        }
+        return false;
+    }
+     
+    public void Place()
+    {
+        Vector3Int positionInt = GridBuildingSystem.current.gridLayout.LocalToCell(transform.position);
+        BoundsInt areaTemp = area;
+        areaTemp.position = positionInt;
+        Placed = true;
+        GridBuildingSystem.current.TakeArea(areaTemp);
+    }
+
 
     #endregion
 }
--- jeu-de-stratégie-tactique/Assets/Script/Karim/Building Scripts/GridBuildingSystem.cs
+++ jeu-de-stratégie-tactique/Assets/Script/Karim/Building Scripts/GridBuildingSystem.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 
 public class GridBuildingSystem : MonoBehaviour
@@ -11,7 +13,16 @@ public class GridBuildingSystem : MonoBehaviour
     public Tilemap MainTilemap;
     public Tilemap TempTilemap;
 
+
     private static Dictionary<TileType, TileBase> tileBases = new Dictionary<TileType, TileBase>();
+
+    private Building temp;
+    private Vector3 prevPos;
+    private BoundsInt prevArea;
+    public TileBase green;
+    public TileBase red;
+    public TileBase white;
+
     #region Unity Methods
     private void Awake()
     {
@@ -19,20 +30,63 @@ public class GridBuildingSystem : MonoBehaviour
     }
     private void Start()
     {
+       
         string tilePath = @"Tiles\";
+        //Tile green = Resources.Load<Tile>(path: tilePath + "green");
         tileBases.Add(TileType.Empty, null);
-        tileBases.Add(TileType.White, Resources.Load<TileBase>(path:tilePath + "white"));
-        tileBases.Add(TileType.Green, Resources.Load<TileBase>(path: tilePath + "green"));
-        tileBases.Add(TileType.Red, Resources.Load<TileBase>(path: tilePath + "red"));
+        tileBases.Add(TileType.White, white);
+        tileBases.Add(TileType.Green, green);
+        tileBases.Add(TileType.Red, red);
+        for (int i = 0; i < tileBases.Count; i++)
+        {
+            Debug.Log(tileBases[TileType.Green]);
+        }
+
+         
     }
 
     private void Update()
     {
+        if (!temp)
+        {
+            return;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject(0))
+            {
+                return;
+            }
 
+            if (!temp.Placed)
+            {
+                Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3Int cellPos = gridLayout.LocalToCell(touchPos);
+
+                if (prevPos != cellPos)
+                {
+                    temp.transform.localPosition = gridLayout.CellToLocalInterpolated(cellPos + new Vector3(0.5f, .5f, 0f));
+                    prevPos = cellPos;
+                    FollowBuilding();
+                }
+            }
+        }
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (temp.CanBePlaced())
+            {
+                temp.Place();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ClearArea();
+            Destroy(temp.gameObject);
+        }
     }
     #endregion
     #region Tilemap Management
-
+    
     private static void FillTiles(TileBase[] arr, TileType type)
     {
         for (int i = 0; i < arr.Length; i++)
@@ -68,8 +122,75 @@ public class GridBuildingSystem : MonoBehaviour
 
     #endregion
     #region Building Placement
+    public void IntitializeWithBuilding(GameObject building)
+    {
+        temp = Instantiate(building, Vector3.zero, Quaternion.identity).GetComponent<Building>();
+        FollowBuilding();
+    }
 
+    private void ClearArea()
+    {
+        TileBase[] toClear = new TileBase[prevArea.size.x * prevArea.size.y * prevArea.size.z];
+        FillTiles(toClear, TileType.White);
+        TempTilemap.SetTilesBlock(prevArea, toClear);
+    }
+
+    private void FollowBuilding()
+    {
+        ClearArea();
+
+        temp.area.position = gridLayout.WorldToCell(temp.gameObject.transform.position);
+  
+        BoundsInt buildingArea = temp.area;
+
+        TileBase[] basesArray = GetTilesBlock(buildingArea, MainTilemap);
+
+        int size = basesArray.Length;
+        for (int i = 0; i < size; i++)
+        {
+            basesArray[i] = tileBases[TileType.Green];
+        }
+        TileBase[] tileArray = new TileBase[size];
+
+        /*for (int i = 0; i < basesArray.Length; i++)
+        {
+            if (basesArray[i] == tileBases[TileType.White])
+            {
+                tileArray[i] = tileBases[TileType.Green];
+            }
+            else
+            {
+                FillTiles(tileArray, TileType.Red);
+                break;
+            }
+        }*/
+
+        
+
+        TempTilemap.SetTilesBlock(buildingArea, basesArray);
+        prevArea = buildingArea;
+    }
     
+    public bool CanTakeArea (BoundsInt area)
+    {
+        TileBase[] baseArray = GetTilesBlock(area, MainTilemap);
+        foreach (var b in baseArray)
+        {
+            if(b != tileBases[TileType.White])
+            {
+                Debug.Log("peut pas mettre ici");
+                return false;
+            }
+        }
+        return true;
+    }
+    public void TakeArea(BoundsInt area)
+    {
+        SetTilesBlock(area, TileType.Empty, TempTilemap);
+        SetTilesBlock(area, TileType.Green, MainTilemap);
+    }
+
+
     #endregion
 
     public enum TileType
