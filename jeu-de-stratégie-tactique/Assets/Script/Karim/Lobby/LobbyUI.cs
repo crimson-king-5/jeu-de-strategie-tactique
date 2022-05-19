b098cf32558ce12f2700cd254564ce1e76f6e816
Karim <karimal-homsi@hotmail.com>
2022-05-19 17:40:33 +0200

    big changes in code

--- jeu-de-stratégie-tactique/Assets/Script/Karim/Lobby/LobbyUI.cs
+++ jeu-de-stratégie-tactique/Assets/Script/Karim/Lobby/LobbyUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.UI;
 using System;
+using DapperDino.UMT.Lobby.Networking;
 
 namespace TEAM2
 {
@@ -15,18 +16,80 @@ namespace TEAM2
         [SerializeField] private Button startGameButton;
 
         private NetworkList<LobbyPlayerState> lobbyPlayers = new NetworkList<LobbyPlayerState>();
+
+
         public override void OnNetworkSpawn()
         {
-            if (NetworkManager.Singleton.IsClient)
+
+            if (IsClient)
             {
                 lobbyPlayers.OnListChanged += HandheldLobbyPlayersStateChanged;
             }
-            if (NetworkManager.Singleton.IsServer)
+            if (IsServer)
             {
+                startGameButton.gameObject.SetActive(true);
 
+                NetworkManager.Singleton.OnClientConnectedCallback += HandlecClientConnected;
+                NetworkManager.Singleton.OnClientDisconnectCallback += HandlecClientDisconnect;
+
+
+                foreach(NetworkClient client in NetworkManager.Singleton.ConnectedClientsList)
+                {
+                    HandlecClientConnected(client.ClientId);
+                }
             }
         }
+        private void OnDestroy()
+        {
+            lobbyPlayers.OnListChanged -= HandheldLobbyPlayersStateChanged;
 
+            if (NetworkManager.Singleton)
+            {
+                NetworkManager.Singleton.OnClientConnectedCallback -= HandlecClientConnected;
+                NetworkManager.Singleton.OnClientDisconnectCallback -= HandlecClientDisconnect;
+
+            }
+
+       
+        }
+
+
+        private bool IsEveroneReady()
+        {
+            if(lobbyPlayers.Count < 2)
+            {
+                return false;
+
+            }
+
+            foreach (var  player in lobbyPlayers)
+            {
+                if (!player.IsReady)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void HandlecClientConnected(ulong clientId)
+        {
+            var playerData= ServerGameNetPortal.Instance.GetPlayerData(clientId);
+
+            if (!playerData.HasValue) { return; }
+
+            lobbyPlayers.Add(new LobbyPlayerState(clientId, 
+                playerData.Value.PlayerName,
+                false
+                ));
+        }
+
+        private void HandlecClientDisconnect(ulong clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+        
         private void HandheldLobbyPlayersStateChanged(NetworkListEvent<LobbyPlayerState> changeEvent)
         {
             throw new NotImplementedException();
